91e7bb61ec2ea250c048d5be9b20d7923de6c3d5
rasheeqqua <rasheeq1018182@gmail.com>
2023-01-31 15:36:13 +0600

    n-th + 3 commit

--- build/index.js
+++ build/index.js
@@ -1,11 +1,2 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var TREE_TYPES;
-(function (TREE_TYPES) {
-    TREE_TYPES["EVENT_TREE"] = "e";
-    TREE_TYPES["EVENT_SEQUENCE_DIAGRAM"] = "e";
-    TREE_TYPES["FAULT_TREE"] = "f";
-    TREE_TYPES["BAYESIAN_NETWORK"] = "b";
-    TREE_TYPES["MANAGEMENT_FAULT_TREE"] = "m";
-    TREE_TYPES["INITIATING_EVENT"] = "i";
-})(TREE_TYPES || (TREE_TYPES = {}));
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,6 @@ import { FaultTreeJSON } from "./fault_tree";
 import { EventTreeJSON } from "./event_tree";
 import { BayesianNetworkJSON } from "./bayesian_network";
 
-enum TREE_TYPES {
-  EVENT_TREE = 'e',
-  EVENT_SEQUENCE_DIAGRAM = 'e',
-  FAULT_TREE = 'f',
-  BAYESIAN_NETWORK = 'b',
-  MANAGEMENT_FAULT_TREE = 'm',
-  INITIATING_EVENT = 'i'
-}
-
 export interface hcl_tree_data {
-  tree_type: TREE_TYPES,
   tree_data: FaultTreeJSON | EventTreeJSON | BayesianNetworkJSON
 }
